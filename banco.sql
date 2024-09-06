25c780a11cf316f709ac7967ba19a877274fb324
Juan Cruz Pineiro <juancruzpineiro@gmail.com>
2024-09-06 12:09:06 -0300

    seguimos

--- banco.sql
+++ banco.sql
@@ -1,4 +1,4 @@
-drop database BANCO;
+-- drop database BANCO;
 
 CREATE DATABASE BANCO CHARACTER SET latin1 COLLATE latin1_spanish_ci;
 USE BANCO;
@@ -42,12 +42,12 @@ CREATE TABLE Empleado(
     direccion VARCHAR(100) NOT NULL,
     telefono VARCHAR(12),
 	cargo VARCHAR(100),
-    
+    nro_suc INT UNSIGNED NOT NULL,
     
     password VARCHAR (32) NOT NULL,-- Ver qué onda con el hash
     
 
-    nro_suc INT UNSIGNED NOT NULL,
+    
     
     PRIMARY KEY(legajo),
     FOREIGN KEY(nro_suc) REFERENCES Sucursal(nro_suc)
@@ -55,7 +55,7 @@ CREATE TABLE Empleado(
 
 -- Cliente (nro cliente, apellido, nombre, tipo doc, nro doc, direccion, telefono, fecha nac)
 CREATE TABLE Cliente(
-	nro_cliente INT UNSIGNED NOT NULL,
+	nro_cliente BIGINT UNSIGNED NOT NULL,
 	CHECK (nro_cliente > 9999 AND nro_cliente <= 100000),
 
 	nombre VARCHAR(100) NOT NULL,
@@ -65,7 +65,7 @@ CREATE TABLE Cliente(
     direccion VARCHAR(100) NOT NULL,
     telefono VARCHAR(12),
 	fecha_nac VARCHAR(100), -- No especifica qué tipo de dato es
-
+	nro_suc INT UNSIGNED NOT NULL,
     
     
     PRIMARY KEY(nro_cliente),
@@ -75,26 +75,43 @@ CREATE TABLE Cliente(
 -- Plazo Fijo (nro plazo, capital, fecha inicio, fecha fin, tasa interes, interes, nro suc)
 -- SIN TERMINAR 
 CREATE TABLE Plazo_Fijo(
-    nro_plazo INT UNSIGNED NOT NULL,
-    CHECK (nro_plazo>9999999 AND nro_plazo <= 10000000),
-    -- PREGUNTAR EL TEMA FECHAS
-    apellido VARCHAR(100) NOT NULL,
-    tipo_doc VARCHAR(10) NOT NULL,
-    nro_doc INT SIGNED NOT NULL, -- VER QUÉ ONDA CON EL RANGO
-    direccion VARCHAR(100) NOT NULL,
-    telefono VARCHAR(12),
-	cargo VARCHAR(100),
+	
+    nro_plazo BIGINT UNSIGNED NOT NULL CHECK (nro_plazo >= 10000000 AND nro_plazo <= 99999999),
+    capital DECIMAL(10, 2) CHECK (capital > 0),
+    fecha_inicio DATE,
+    fecha_n DATE,
+    tasa_interes DECIMAL(5, 2) CHECK (tasa_interes > 0), -- Real positivo con 2 decimales
+    interes DECIMAL(10, 2) CHECK (interes > 0), -- Real positivo con 2 decimales
+ 
     
-    
-    password VARCHAR (32) NOT NULL,-- Ver qué onda con el hash
-    
-
+	
     nro_suc INT UNSIGNED NOT NULL,
     
-    PRIMARY KEY(legajo),
+    PRIMARY KEY(nro_plazo),
     FOREIGN KEY(nro_suc) REFERENCES Sucursal(nro_suc)
 ) ENGINE=InnoDB;
 
+CREATE TABLE Tasa_Plazo_Fijo (
+    periodo INT UNSIGNED NOT NULL
+    CHECK (periodo >= 100 AND periodo <= 999), 
+    monto_inf DECIMAL(10, 2)  
+    CHECK (monto_inf > 0),
+    monto_sup DECIMAL(10, 2)  
+    CHECK (monto_sup > 0), 
+    
+    tasa DECIMAL(5, 2)
+    CHECK (tasa > 0),
+    
+    PRIMARY KEY (periodo, monto_inf)
+) ENGINE=InnoDB;
 
--- insert into Cuidad values (8000,"Bahia Blanca");
+CREATE TABLE Plazo_Cliente (
+    nro_plazo BIGINT UNSIGNED NOT NULL, 
+    nro_cliente BIGINT UNSIGNED NOT NULL, 
+    PRIMARY KEY (nro_plazo, nro_cliente),
+    FOREIGN KEY (nro_plazo) REFERENCES Plazo_Fijo(nro_plazo), 
+    FOREIGN KEY (nro_cliente) REFERENCES Cliente(nro_cliente) 
+);
+
+
 	
