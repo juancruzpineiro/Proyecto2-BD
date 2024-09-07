033913d76e0d0de10901562fc092f2ef3ad0270d
sahid <144352266+SahidDulce@users.noreply.github.com>
2024-09-07 16:28:14 -0300

    Add files via upload
    corregi errores

--- banco.sql
+++ banco.sql
@@ -9,6 +9,8 @@ CREATE TABLE Ciudad(
     CHECK (cod_postal > 999 AND cod_postal <= 9999),
     -- PREGUNTAR SI ES ASÍ LO QUE PIDEN, 4 DÍGITOS O CHAR
     nombre VARCHAR(100) NOT NULL,
+
+    CONSTRAINT pk_cod_postal
     PRIMARY KEY(cod_postal)
 ) ENGINE=InnoDB;
 
@@ -20,10 +22,13 @@ CREATE TABLE Sucursal(
     nombre VARCHAR(100) NOT NULL,
     direccion VARCHAR(100) NOT NULL,
     telefono VARCHAR(12),
-	horario VARCHAR(4),
+	horario VARCHAR(5),
     cod_postal INT UNSIGNED NOT NULL,
     
+    CONSTRAINT pk_suc
     PRIMARY KEY(nro_suc),
+
+    CONSTRAINT fk_sucursal_cod_postal
     FOREIGN KEY(cod_postal) REFERENCES Ciudad(cod_postal)
 ) ENGINE=InnoDB;
 
@@ -38,18 +43,21 @@ CREATE TABLE Empleado(
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     tipo_doc VARCHAR(10) NOT NULL,
-    nro_doc INT SIGNED NOT NULL, -- VER QUÉ ONDA CON EL RANGO
+    nro_doc INT UNSIGNED NOT NULL,
+    CHECK (nro_doc >= 10000000 AND nro_doc <= 99999999),
     direccion VARCHAR(100) NOT NULL,
     telefono VARCHAR(12),
 	cargo VARCHAR(100),
     nro_suc INT UNSIGNED NOT NULL,
     
-    password VARCHAR (32) NOT NULL,-- Ver qué onda con el hash
+    password VARCHAR(32) NOT NULL,-- Ver qué onda con el hash
     
 
     
-    
+    CONSTRAINT pk_legajo
     PRIMARY KEY(legajo),
+
+    CONSTRAINT fk_empleado_nro_suc
     FOREIGN KEY(nro_suc) REFERENCES Sucursal(nro_suc)
 ) ENGINE=InnoDB;
 
@@ -57,19 +65,21 @@ CREATE TABLE Empleado(
 CREATE TABLE Cliente(
 	nro_cliente BIGINT UNSIGNED NOT NULL,
 	CHECK (nro_cliente > 9999 AND nro_cliente <= 100000),
-
 	nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     tipo_doc VARCHAR(10) NOT NULL,
-	nro_doc INT SIGNED NOT NULL, -- VER QUÉ ONDA CON EL RANGO, 8 CIFRAS SEGÚN EL ENUNCIADO
+    nro_doc INT SIGNED NOT NULL,
+    CHECK (nro_doc >= 10000000 AND nro_doc <= 99999999),
     direccion VARCHAR(100) NOT NULL,
     telefono VARCHAR(12),
 	fecha_nac VARCHAR(100), -- No especifica qué tipo de dato es
 	nro_suc INT UNSIGNED NOT NULL,
     
-    
+    CONSTRAINT pk_nro_cliente
     PRIMARY KEY(nro_cliente),
-    FOREIGN KEY(nro_suc) REFERENCES Sucursal(nro_suc)
+
+    CONSTRAINT fk_cliente_nro_suc
+    FOREIGN KEY(nro_suc) REFERENCES Sucursal(nro_suc) -- por que numero de sucursal foreign key?
 ) ENGINE=InnoDB;
 
 -- Plazo Fijo (nro plazo, capital, fecha inicio, fecha fin, tasa interes, interes, nro suc)
@@ -79,15 +89,18 @@ CREATE TABLE Plazo_Fijo(
     nro_plazo BIGINT UNSIGNED NOT NULL CHECK (nro_plazo >= 10000000 AND nro_plazo <= 99999999),
     capital DECIMAL(10, 2) CHECK (capital > 0),
     fecha_inicio DATE,
-    fecha_n DATE,
+    fecha_fin DATE,
     tasa_interes DECIMAL(5, 2) CHECK (tasa_interes > 0), -- Real positivo con 2 decimales
-    interes DECIMAL(10, 2) CHECK (interes > 0), -- Real positivo con 2 decimales
+    interes DECIMAL(10, 2) CHECK (interes >= 0), -- Real positivo con 2 decimales
  
     
 	
     nro_suc INT UNSIGNED NOT NULL,
     
+    CONSTRAINT pk_nro_plazo
     PRIMARY KEY(nro_plazo),
+
+    CONSTRAINT fk_plazo_fijo_nro_suc
     FOREIGN KEY(nro_suc) REFERENCES Sucursal(nro_suc)
 ) ENGINE=InnoDB;
 
@@ -98,18 +111,27 @@ CREATE TABLE Tasa_Plazo_Fijo (
     CHECK (monto_inf > 0),
     monto_sup DECIMAL(10, 2)  
     CHECK (monto_sup > 0), 
-    
     tasa DECIMAL(5, 2)
     CHECK (tasa > 0),
     
-    PRIMARY KEY (periodo, monto_inf)
+    CONSTRAINT pk_periodo
+    PRIMARY KEY (periodo),  -- PREGUNTAR CUAL ES LA LLAVE PRIMARIO O SI SON LOS tres;
+
+    KEY(monto_inf),
+    KEY(monto_sup)
 ) ENGINE=InnoDB;
 
 CREATE TABLE Plazo_Cliente (
-    nro_plazo BIGINT UNSIGNED NOT NULL, 
-    nro_cliente BIGINT UNSIGNED NOT NULL, 
-    PRIMARY KEY (nro_plazo, nro_cliente),
-    FOREIGN KEY (nro_plazo) REFERENCES Plazo_Fijo(nro_plazo), 
+    nro_plazo BIGINT UNSIGNED NOT NULL CHECK (nro_plazo >= 10000000 AND nro_plazo <= 99999999), 
+    nro_cliente BIGINT UNSIGNED NOT NULL,
+	CHECK (nro_cliente > 9999 AND nro_cliente <= 100000),
+    
+    CONSTRAINT pk_plazo_cliente PRIMARY KEY (nro_plazo, nro_cliente),  -- Clave primaria compuesta,CONSULTAR
+
+    CONSTRAINT fk_plazo_cliente_nro_plazo_plazo_fijo
+    FOREIGN KEY (nro_plazo) REFERENCES Plazo_Fijo(nro_plazo),
+
+    CONSTRAINT fk_plazo_cliente_nro_cliente_cliente 
     FOREIGN KEY (nro_cliente) REFERENCES Cliente(nro_cliente) 
 );
 
